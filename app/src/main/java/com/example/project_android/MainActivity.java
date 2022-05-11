300efb59d6a40086bc6c8368d5b2dd5e8c90e3e4
phatngoc0711 <phat07112001@gmail.com>
2022-05-11 11:50:07 +0700

    firebase

--- app/src/main/java/com/example/project_android/MainActivity.java
+++ app/src/main/java/com/example/project_android/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.project_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSignIn, btnSignUp;
+    TextView slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        slogan = (TextView) findViewById(R.id.textSlogan);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Nabila.ttf");
+
+        slogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/project_android/SignUp.java
@@ -0,0 +1,76 @@
+package com.example.project_android;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.project_android.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SignUp extends AppCompatActivity {
+    EditText edtPhone, edtName, edtPass;
+    Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtPhone = (EditText) findViewById(R.id.edtPhone);
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog progressDialog = new ProgressDialog(SignUp.this);
+                progressDialog.setMessage("Please waiting....");
+                progressDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.child(edtPhone.getText().toString()).exists()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(SignUp.this, "Phone Number Already Register!", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            progressDialog.dismiss();
+                            User user = new User(edtName.getText().toString(), edtPass.getText().toString());
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Sign Up Success!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+
+                    }
+                });
+
+
+
+            }
+        });
+
+    }
+}
