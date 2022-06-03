ddc33e7d1355d33ce4a839cbc77b49481d34a44a
cuadong <thanhphuoc01012001@gmail.com>
2022-06-03 16:40:39 +0700

    thêm currentRequest, BASE_URL, getFCMClient

--- app/src/main/java/com/example/project_android/Common/Common.java
+++ app/src/main/java/com/example/project_android/Common/Common.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import com.example.project_android.Model.Request;
 import com.example.project_android.Model.User;
+import com.example.project_android.Remote.APIService;
+import com.example.project_android.Remote.RetrofitClient;
 
 public class Common {
     public static User currentUser;
     public static final String  DELETE = "Delete";
     public static final String  USER_KEY = "User";
     public static final String  PWD_KEY = "Password";
+    public static Request currentRequest;
+    public static final String BASE_URL = "https://fcm.googleapis.com/";
 
     public static boolean isConnectedToInternet(Context context)
     {
@@ -29,5 +34,7 @@ public class Common {
         }
         return false;
     }
-
+    public static APIService getFCMClient() {
+        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
+    }
 }
